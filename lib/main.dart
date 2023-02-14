e70d6611cfd259896b361df5f47fe2703473e1b1
vwBanditwv <nils.wacker@mail.de>
2023-02-14 16:56:13 +0100

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:dart_appwrite/dart_appwrite.dart';
+
+/*
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - request body data as a string
+    'variables' - object with function variables
+
+  'res' variable has:
+    'send(text, status: status)' - function to return text response. Status code defaults to 200
+    'json(obj, status: status)' - function to return JSON response. Status code defaults to 200
+  
+  If an error is thrown, a response with code 500 will be returned.
+*/
+
+Future<void> start(final req, final res) async {
+  final client = Client();
+
+  // Uncomment the services you need, delete the ones you don't
+  // final account = Account(client);
+  // final avatars = Avatars(client);
+  final database = Databases(client);
+  // final functions = Functions(client);
+  // final health = Health(client);
+  // final locale = Locale(client);
+  // final storage = Storage(client);
+  // final teams = Teams(client);
+  // final users = Users(client);
+
+  if (req.variables['APPWRITE_FUNCTION_ENDPOINT'] == null ||
+      req.variables['APPWRITE_FUNCTION_API_KEY'] == null) {
+    print(
+        "Environment variables are not set. Function cannot use Appwrite SDK.");
+  } else {
+    client
+        .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
+        .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
+        .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
+        .setSelfSigned(status: true);
+
+    int count = 0;
+    int innercount = 0;
+    do {
+      await database
+          .listDocuments(
+        databaseId: req.variables['DATABASE_ID'],
+        collectionId: req.variables['COLLECTION_ID'],
+      )
+          .then((value) {
+        count = count + value.documents.length;
+        innercount = value.documents.length;
+        value.documents.forEach((element) {
+          database.deleteDocument(
+              databaseId: req.variables['DATABASE_ID'],
+              collectionId: req.variables['COLLECTION_ID'],
+              documentId: element.$id);
+        });
+      }).onError((error, stackTrace) {
+        res.json({'error': error.toString()});
+      });
+    } while (innercount > 0);
+    res.json({'total documents deleted': count});
+  }
+}
